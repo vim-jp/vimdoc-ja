61284ef8884d58936a61eb8afebec1cc1a6b51f5
vimdoc-ja master <vimdoc-ja@vim-jp.org>
2013-10-06 13:07:02 +0900

    modularize

--- tools/master-update.sh
+++ tools/master-update.sh
@@ -1,55 +1,22 @@
 #!/bin/sh
 # vim:set sts=2 sw=2 tw=0 et:
 
-GIT_REPO="git@github.com:vim-jp/vimdoc-ja.git"
-GIT_BRANCH="master2-test"
-GIT_NAME="vimdoc-ja system"
-GIT_EMAIL="vimdoc-ja@vim-jp.org"
+. `dirname $0`/update-lib.sh
 
-WORKDIR="target"
-BRANCHDIR="${WORKDIR}/${GIT_BRANCH}"
+setup "master2-test"
 
-# Prepare and clean up working directories.
-if [ ! -e "${WORKDIR}" ] ; then
-  mkdir -p "${WORKDIR}"
+# TODO: update repository.
+touch test.txt
+ts=`date "+%Y/%m/%d %H:%M:%S%z"`
+echo "${ts}" >> test.txt
+if [ -e foo.txt ] ; then
+  rm -f foo.txt
+  touch bar.txt
+  echo "${ts} foo->bar" >> bar.txt
+elif [ -e bar.txt ] ; then
+  rm -f bar.txt
+  touch foo.txt
+  echo "${ts} bar->foo" >> foo.txt
 fi
-if [ -e "${BRANCHDIR}" ] ; then
-  rm -rf "${BRANCHDIR}"
-fi
 
-# Checkout a target branch.
-echo "Checking out \"${GIT_BRANCH}\" from \"${GIT_REPO}\""
-git clone -q -b "${GIT_BRANCH}" --depth 5 "${GIT_REPO}" "${BRANCHDIR}" || exit 1
-
-# work in BRANCHDIR.
-(
-  echo "Make changes in \"${BRANCHDIR}\""
-  cd "${BRANCHDIR}"
-  # TODO: update buffers
-  touch test.txt
-  ts=`date "+%Y/%m/%d %H:%M:%S%z"`
-  echo "${ts}" >> test.txt
-  if [ -e foo.txt ] ; then
-    rm -f foo.txt
-    touch bar.txt
-    echo "${ts} foo->bar" >> bar.txt
-  elif [ -e bar.txt ] ; then
-    rm -f bar.txt
-    touch foo.txt
-    echo "${ts} bar->foo" >> foo.txt
-  fi
-)
-
-# Commit changes
-(
-  echo "Register changes and push it to \"${GIT_REPO}\""
-  sha1hash=`git show --pretty="format:%H" | head -1`
-  commit_message="Deployed ${sha1hash} from ${GIT_BRANCH} in ${GIT_REPO}"
-  cd "${BRANCHDIR}"
-  host=`hostname`
-  git config user.name "${GIT_NAME}"
-  git config user.email "${GIT_EMAIL}"
-  git add --all .
-  git commit -m "${commit_message}"
-  git push || exit 1
-)
+teardown "vim-jp/vimdoc-ja@"
---
+++ tools/update-lib.sh
@@ -0,0 +1,55 @@
+CONFIGFILE=`dirname $0`/update-config.sh
+
+. "${CONFIGFILE}"
+
+if [ x"${GIT_REPO}" = "x" ] ; then
+  echo "GIT_REPO is missing in \"${CONFIGFILE}\"" && exit 1
+fi
+if [ x"${GIT_NAME}" = "x" ] ; then
+  echo "GIT_NAME is missing in \"${CONFIGFILE}\"" && exit 1
+fi
+if [ x"${GIT_EMAIL}" = "x" ] ; then
+  echo "GIT_EMAIL is missing in \"${CONFIGFILE}\"" && exit 1
+fi
+
+setup() {
+  GIT_BRANCH="$1" ; shift
+  if [ x"${GIT_BRANCH}" = "x" ] ; then
+    echo "GIT_BRANCH is required"
+    exit 1
+  fi
+
+  WORKDIR="target"
+  BRANCHDIR="${WORKDIR}/${GIT_BRANCH}"
+
+  # FIXME: exit if failed.
+  SHA1HASH=`git show --pretty="format:%H" | head -1`
+
+  # Prepare and clean up working directories.
+  if [ ! -e "${WORKDIR}" ] ; then
+    mkdir -p "${WORKDIR}"
+  fi
+  if [ -e "${BRANCHDIR}" ] ; then
+    rm -rf "${BRANCHDIR}"
+  fi
+
+  # Checkout a target branch.
+  echo "Checking out \"${GIT_BRANCH}\" from \"${GIT_REPO}\""
+  git clone -q -b "${GIT_BRANCH}" --depth 5 "${GIT_REPO}" "${BRANCHDIR}" || exit 1
+
+  echo "Make changes in \"${BRANCHDIR}\""
+  cd "${BRANCHDIR}"
+  _gitdir=`pwd`
+}
+
+teardown() {
+  HASH_PREFIX=$1 ; shift
+  echo "Register changes and push it to \"${GIT_REPO}\""
+  cd "${_gitdir}"
+  git config user.name "${GIT_NAME}"
+  git config user.email "${GIT_EMAIL}"
+  git add --all .
+  commit_message="Deployed ${HASH_PREFIX}${SHA1HASH} by `hostname`"
+  git commit -m "${commit_message}"
+  git push || exit 1
+}
